7105c6349d9861a6d04863a8d7471938c32eb48d
l-marcel <lmgh1312@gmail.com>
2025-04-01 10:15:53 -0300

    [feat] Conectando banco de dados

--- backend/src/main/java/app/hakai/backend/models/Game.java
+++ backend/src/main/java/app/hakai/backend/models/Game.java
@@ -3,15 +3,24 @@ package app.hakai.backend.models;
 import java.util.LinkedList;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 public class Game {
     @Getter
     @Setter
+    @Id
+    @GeneratedValue
     private UUID uuid;
 
     @Getter
     @Setter
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL, orphanRemoval = true)
     private LinkedList<Question> questions = new LinkedList<Question>();
 };
--- backend/src/main/java/app/hakai/backend/models/Question.java
+++ backend/src/main/java/app/hakai/backend/models/Question.java
@@ -1,5 +1,18 @@
 package app.hakai.backend.models;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Question {
-    
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue
+    private UUID uuid;
 };

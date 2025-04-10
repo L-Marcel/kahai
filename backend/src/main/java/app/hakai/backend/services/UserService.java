b7e551fcbdebdd103698f4f39f604dc4a8b7cf8e
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2025-04-10 09:46:23 -0300

    [feat] Implementação do registro de usuário

--- backend/src/main/java/app/hakai/backend/services/UserService.java
+++ backend/src/main/java/app/hakai/backend/services/UserService.java
@@ -1,5 +1,7 @@
 package app.hakai.backend.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,23 @@ public class UserService {
 
     }
 
-    public void registrarUsuario(User usuarioDTO) {
+    public void registerUser(User usuarioDTO) {
         userRepository.save(usuarioDTO);
+    }
+
+    public User login(String email, String senha) throws Exception {
+        Optional<User> optionalUser = userRepository.findById(email);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+
+            if (user.getSenha().equals(senha)) {
+                return user; // Login bem-sucedido
+            } else {
+                throw new Exception("Senha incorreta");
+            }
+        } else {
+            throw new Exception("Usuário não encontrado");
+        }
     }
 }

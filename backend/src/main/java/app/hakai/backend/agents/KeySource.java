124a2ba4b73b1efa79fcc320c2cf72a8c58abad3
l-marcel <lmgh1312@gmail.com>
2025-04-17 10:45:32 -0300

    [feat] Implementando rotação de chaves

--- backend/src/main/java/app/hakai/backend/agents/KeySource.java
+++ backend/src/main/java/app/hakai/backend/agents/KeySource.java
@@ -2,4 +2,5 @@ package app.hakai.backend.agents;
 
 public interface KeySource {
     public String getKey();
+    public String[] getKeys();
 };
--- backend/src/main/java/app/hakai/backend/config/WebConfig.java
+++ backend/src/main/java/app/hakai/backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package app.hakai.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import app.hakai.backend.converters.StringToUuidConverter;
 
 @Configuration
+@PropertySource("classpath:secret.properties")
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(

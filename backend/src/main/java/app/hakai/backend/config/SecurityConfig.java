ea3064c5123e20fc4ed73481f93d378313c14e11
l-marcel <lmgh1312@gmail.com>
2025-05-07 13:57:28 -0300

    [feat] Adicionando uso dinâmico do ngrok

--- backend/src/main/java/app/hakai/backend/config/SecurityConfig.java
+++ backend/src/main/java/app/hakai/backend/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package app.hakai.backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -17,10 +21,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .cors(Customizer.withDefaults()) // Habilita o CORS
-            .csrf(csrf -> csrf.disable()) // Desativa CSRF
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) 
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(
-                auth -> auth.anyRequest().permitAll() // Permite todas as requisições
+                auth -> auth.anyRequest().permitAll()
             ).build();
     };
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));;
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 };

941bc733325a185011a2ef30fd3b5dbbeb433428
Maria Clara Fernandes <fernandes.mcfo@gmail.com>
2025-05-12 11:13:30 -0300

    [feat] Adicionando validações Registro e Login

--- backend/src/main/java/app/hakai/backend/errors/UserNotFound.java
+++
@@ -1,9 +0,0 @@
-package app.hakai.backend.errors;
-
-import org.springframework.http.HttpStatus;
-
-public class UserNotFound extends HttpError {
-    public UserNotFound() {
-        super("Usuário não encontrado!", HttpStatus.NOT_FOUND);
-    };
-};
--- backend/src/main/java/app/hakai/backend/services/UserService.java
+++ backend/src/main/java/app/hakai/backend/services/UserService.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Service;
 
 import app.hakai.backend.auth.JwtUtil;
 import app.hakai.backend.errors.EmailAlreadyInUse;
-import app.hakai.backend.errors.UserNotFound;
+import app.hakai.backend.errors.InvalidCredentials;
+import app.hakai.backend.errors.InvalidEmail;
+import app.hakai.backend.errors.MissingFields;
+import app.hakai.backend.errors.WeakPassword;
 import app.hakai.backend.models.User;
 import app.hakai.backend.repositories.UsersRepository;
 
@@ -22,19 +25,45 @@ public class UserService {
     private JwtUtil jwtUtil;
 
     public void register(String email, String password, String name) {
-        if (repository.findByEmail(email).isPresent())
+        if (email == null || email.isBlank() ||
+                password == null || password.isBlank() ||
+                name == null || name.isBlank()) {
+            throw new MissingFields();
+        }
+
+        if (!email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+            throw new InvalidEmail();
+        }
+
+        if (password.length() < 6) {
+            throw new WeakPassword();
+        }
+
+        if (repository.findByEmail(email).isPresent()) {
             throw new EmailAlreadyInUse();
+        }
+
         User user = new User(email, encoder.encode(password), name);
         repository.save(user);
-    };
+    }
 
-    public String login(String email, String password) throws UserNotFound {
-        User user = repository.findByEmail(email).orElseThrow(
-                () -> new UserNotFound());
-        if (!encoder.matches(password, user.getPassword()))
-            throw new UserNotFound();
+    public String login(String email, String password) {
+        if (email == null || email.isBlank() || password == null || password.isBlank()) {
+            throw new MissingFields();
+        }
+
+        if (!email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+            throw new InvalidEmail();
+        }
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(InvalidCredentials::new);
+
+        if (!encoder.matches(password, user.getPassword())) {
+            throw new InvalidCredentials();
+        }
 
         return jwtUtil.generateToken(user.getUuid());
-    };
+    }
 
 };

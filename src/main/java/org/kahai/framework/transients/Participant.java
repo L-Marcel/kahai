753cf8fe601891ac48baf6b4143894fefd54a044
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2025-07-03 13:19:13 -0300

    [feat] Adicionando Strategi para variação de score

--- src/main/java/org/kahai/framework/transients/Participant.java
+++ src/main/java/org/kahai/framework/transients/Participant.java
@@ -7,15 +7,17 @@ import org.kahai.framework.models.Difficulty;
 import org.kahai.framework.models.User;
 
 import lombok.Getter;
+import lombok.Setter;
 
-@Getter 
+@Getter
+@Setter
 public final class Participant {
     private UUID uuid = UUID.randomUUID();
     private Room room;
     private Optional<User> user;
     private String nickname;
     private Difficulty currentDifficulty = Difficulty.NORMAL;
-    
+
     private Integer correctAnswers = 0;
     private Integer wrongAnswers = 0;
     private Integer score = 0;
@@ -57,16 +59,16 @@ public final class Participant {
     };
 
     public Difficulty setNetxDifficulty(
-        Boolean isCorrect
-    ) {
+            Boolean isCorrect) {
         Difficulty[] difficulties = Difficulty.values();
         Integer index = currentDifficulty.ordinal();
 
-        if(isCorrect && index < difficulties.length - 1) {
+        if (isCorrect && index < difficulties.length - 1) {
             return currentDifficulty = difficulties[index + 1];
-        } else if(!isCorrect && index > 0) {
+        } else if (!isCorrect && index > 0) {
             return currentDifficulty = difficulties[index - 1];
-        };
+        }
+        ;
 
         return currentDifficulty;
     };
